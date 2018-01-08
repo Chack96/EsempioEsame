2765580eae0c3d0e9928487b0f8efc3d19a163d5
Chack96 <francesco.loda@studenti.unitn.it>
2018-01-08 17:06:04 +0100

    dd

--- server.node.js
+++ server.node.js
@@ -1,12 +1,12 @@
-var porta = 8080;
+var porta = process.env.PORT || 3000;
 var http = require('http');
 var fs = require('fs');
 
 fs.readFile('./index.html', function (err, html) {
-    if (err) throw err;    
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
+    if (err) throw err;
+    http.createServer(function(request, response) {
+        response.writeHeader(200, {"Content-Type": "text/html"});
+        response.write(html);
+        response.end();
     }).listen(porta);
 });
