449af5c36b6209429963269c3da2e8579bf785a6
Chack96 <francesco.loda@studenti.unitn.it>
2018-01-08 17:15:51 +0100

    ddfd

--- server.node.js
+++ server.node.js
@@ -2,7 +2,7 @@ const app = require('./app.node.js');
 
 //apro server su porta 8080
 //heroku vuole ascoltare sulla sua porta
-var porta = process.env.PORT || 3000;
+var porta = process.env.PORT || 5000;
 //var porta = 8080;
 app.listen(porta, function () {
     console.log('Server aperto');
